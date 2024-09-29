cd8728536f231231d2786ac817cdb11276f61c91
Ravivarma <ryuk.noir@gmail.com>
2024-09-29 11:59:53 +0530

    changed the title logo & removed unused styles in headers

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import '../css/Header.css';
-import MovieIcon from '../assets/movie-phobics.svg';
+import MovieIcon from '../assets/movie-phobics.png';
 
 const Header = ({ searchQuery, onSearchChange, onTitleClick }) => {
-    let title = 'MOVIE PHOBICS';
-
     return (
         <header className="header">
             <div className="header-content">
-                <img src={MovieIcon} alt="Movie Phobics Icon" className="header-icon" />
-                <h1 className="header-title" onClick={onTitleClick}>{title}</h1>
+                <img className="header-title" onClick={onTitleClick} src={MovieIcon} alt="Movie Phobics Icon" />
             </div>
             <div className="header-right">
                 <div className="search-container">

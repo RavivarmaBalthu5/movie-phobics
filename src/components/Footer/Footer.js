27ecb1e01d592d276f42e8fa4a6434ae758f22fd
Ravivarma <ryuk.noir@gmail.com>
2024-10-20 21:07:21 +0530

    added option to report any issues

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import '../../css/Footer.css';
 import { fetchGitVersion } from '../../services/movieService';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { GITHUB_URL_ISSUES } from '../../utils/configs';
 
 const Footer = () => {
     const [version, setVersion] = useState('1.0.0');
@@ -19,6 +22,10 @@ const Footer = () => {
         <footer className="footer">
             <div className="footer-details">
                 <p>© 2024 Movie Phobics. All rights reserved. {version}</p>
+                <p>For any issues, Please Report here : <a href={GITHUB_URL_ISSUES} target='_blank' rel="noreferrer">
+                    <FontAwesomeIcon className='icon' icon={faGithub} />
+                </a>
+                </p>
             </div>
         </footer>
     );

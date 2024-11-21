10ea822027455165f3d1302dbb3dc46b1376d665
Ravivarma <ryuk.noir@gmail.com>
2024-11-21 22:24:51 +0530

    updated logo & removed user icon

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import '../../css/Header.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser } from '@fortawesome/free-regular-svg-icons';
+// import { faUser } from '@fortawesome/free-regular-svg-icons';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import logo from '../../assets/moviephobics.png';
 
 const Header = ({ toggleSearch, onTitleClick }) => {
     return (
         <header className="header">
             <div className="header-content" onClick={onTitleClick}>
+                <img src={logo} alt="moviephobics logo" className='logo' />
                 <h2>MOVIE PHOBICS</h2>
             </div>
             <div className="header-right">
-                <FontAwesomeIcon className='icon' icon={faUser} size="2x" />
+                {/* <FontAwesomeIcon className='icon' icon={faUser} size="2x" /> */}
                 <FontAwesomeIcon
                     className='icon'
                     icon={faSearch}

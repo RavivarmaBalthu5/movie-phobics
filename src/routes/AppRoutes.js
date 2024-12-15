26e65fe8d8097d5880dfcd231752ddda4744d1dc
Ravivarma <ryuk.noir@gmail.com>
2024-12-15 22:59:38 +0530

    added new page for 404 errors and route not found

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import MoviesSection from '../components/Movies/MoviesSection';
 import MovieDetails from '../components/Movies/MovieDetails';
+import NotFound from '../components/Common/NotFound';
 
 const AppRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<MoviesSection />} />
             <Route path="/movies/:id" element={<MovieDetails />} />
+            <Route path="*" element={<NotFound />} />
         </Routes>
     );
 };
